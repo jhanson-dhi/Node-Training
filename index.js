4c0557b86d3a899e1aea575a771bde849d153893
John Hanson <john.hanson@dhigroupinc.com>
2016-12-09 12:08:19 -0600

    v4

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req,res) {
+ res.send('hello world');
+});
+
+app.get('/users/', function (req, res) {
+   res.send ('hello users');
+});
+
+app.get('/users/id', function (req, res) {
+   res.send ('hello user id');
+});
+
+app.listen(8081, function() {console.log("we are live.")} );
+
+
+
+
+
+/****
 var http = require("http");
 var url = require("url");
 
 http.createServer(function (req, res) {
-    var queryData = url.parse(req.url, true).query;
+    
+    var queryData = url.parse(req.url);
+    var pth = queryData.pathname.split("/");
+    console.log(pth);
     
     res.writeHead(200, {'Content-Type': 'text/plain'});
     
-    if(queryData.id) {
-     res.end('Hello user: ' + queryData.id + '\n');
-    } else {
-     res.end('Unknown Request\n');
-    }
-    
-    //res.end('Hello World.<br>');
+    res.end("hit the end");
+
 }
 ).listen(8081);
 console.log("Server Running....");
+****/
