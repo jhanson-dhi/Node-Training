ec1c44d167d068aa1a6f6ce20303087284c73a0f
John Hanson <john.hanson@dhigroupinc.com>
2016-12-09 12:29:44 -0600

    v7

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 var express = require('express');
 var app = express();
-
+var userdb = require('./users.json');
 
 
 app.get('/users/', function (req, res) {
-   res.status(200).send ('hello users');
+   res.type('json').status(200).send (userdb);
 });
 
 app.get('/users/:id', function (req, res) {
-   res.status(200).send ('hello user id: ' + req.params.id);
+   var user = userdb.filter(function(obj) {
+    return obj.id == req.params.id;
+   })[0];
+   
+   if (user != null) {
+    res.type('json').status(200).send(user);
+   } else {
+    res.status(404).send('404: user not found.');
+   }
+   
 });
 
 app.use('*', function(req,res){
